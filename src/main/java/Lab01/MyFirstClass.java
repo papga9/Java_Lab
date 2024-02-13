86cdb363fa94926f7445e77d8fe8b845915c2783
Gábor Richárd Papp <gabor.richard.papp@gmail.com>
2024-02-13 17:47:08 +0100

    Final for the day

--- src/main/java/Lab01/MyFirstClass.java
+++ src/main/java/Lab01/MyFirstClass.java
@@ -3,6 +3,26 @@ package Lab01;
 public class MyFirstClass {
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        String paratlan = "";
+        StringBuilder builder = new StringBuilder();
+
+        for (int i=1; i<=9; i+=2) {
+            //paratlan += i + " ";
+            builder.append(" " + i);
+        }
+
+        System.out.println(builder);
+
+        String[][] myTable = new String[10][20];
+
+        for (int i =0; i<myTable.length; i++) {
+            for (int j=0; j<myTable[i].length; j++) {
+                myTable[i][j] = i + "x" + j;
+                System.out.print(myTable[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
     }
 }
