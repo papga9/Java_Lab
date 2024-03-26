56eecb156e2dc163a7af3b29d6d5cabb2b3fb192
Gábor Richárd Papp <gabor.richard.papp@gmail.com>
2024-03-26 17:45:33 +0100

    Lab06 end

--- src/main/java/Lab06/Main.java
+++ src/main/java/Lab06/Main.java
@@ -1,5 +1,14 @@
 package Lab06;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -19,10 +28,38 @@ public class Main {
 
         classes.put("Java1", java1);
         classes.put("Java2", java2);
-        
 
+        try (ObjectOutputStream out = new ObjectOutputStream(Files.newOutputStream(Paths.get("kurzusok.dat")))
+
+        ) {
+            out.writeObject(classes);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (ObjectInputStream in = new ObjectInputStream(Files.newInputStream(Paths.get("kurzusok.dat")))) {
+            Map<String, List<Student>> o = (Map<String, List<Student>>) in.readObject();
+            System.out.println(o);
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        /// nem szép kiírás
+        new Gson().toJson(classes);
+
+        ///szép kiírás
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        System.out.println(gson.toJson(classes));
+
+        try (BufferedReader in = Files.newBufferedReader(Paths.get("gsontest.json"))) {
+            Type typeToRead = new TypeToken<HashMap<String, ArrayList<Student>>>(){}.getType();
+            Map<String, List<Student>> o2 = new Gson().fromJson(in, typeToRead);
+            System.out.println("===" + o2);
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
-
-
 }
--- src/main/java/Lab06/Student.java
+++ src/main/java/Lab06/Student.java
@@ -1,11 +1,21 @@
 package Lab06;
 
-public class Student {
+import java.io.Serializable;
+
+public class Student implements Serializable {
     private String name;
     private String Neptun;
 
     public Student(String name, String neptun) {
         this.name = name;
         Neptun = neptun;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", Neptun='" + Neptun + '\'' +
+                '}';
     }
 }
